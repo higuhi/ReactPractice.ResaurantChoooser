8fb24e7d34204b8a7bf93b8df0b8d4b2b2fdd4df
Hiroshi <hiroshi.garage.sale@gmail.com>
2020-07-21 11:26:48 +0200

    1) Implemented custom buttons and custom text input 2) Enabled prop-types

--- screens/RestaurantsScreen.js
+++ screens/RestaurantsScreen.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Constants } from "expo";
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from '../components/CustomButton.js';
+import CustomTextInput from '../components/CustomTextInput.js';
+
 
 const styles = StyleSheet.create({
     container: {
@@ -11,8 +14,22 @@ const styles = StyleSheet.create({
     },
   });
 
-const RestaurantsScreen = () => {
-    return <View style={styles.container}><Text>This is Restaurants Screen</Text></View>;
+class RestaurantsScreen extends Component { 
+    constructor(props) {
+      super(props);
+      this.state = { inText: "" }
+    }
+
+    render() {
+      return (
+    
+        <View style={styles.container}>
+            <Text>This is Restaurants Screen - {this.state.inText} </Text>
+            <CustomButton text="Hello" onPress={ () => {console.log("botton clicked")} } />
+            <CustomTextInput label="Label" stateHolder={this} stateFieldName="inText" />
+        </View>
+      );  
+    }
 }
 
 exports.RestaurantsScreen = RestaurantsScreen;
